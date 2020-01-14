e4c41c759e1a9eaad154dbc4811f4ccde19a7685
Michael Romay <amerror@gmail.com>
2020-01-13 17:54:57 -0800

    Initial push

---
+++ app.js
@@ -0,0 +1,19 @@
+import isYes from './is-yes.js';
+
+const quizButton = document.getElementById('quizbutton');
+const total = document.getElementById('total');
+
+quizButton.addEventListener('click', () => {
+    // console.log('button works');
+    const name = prompt('What\'s yer name?');
+    const confirmation = confirm(`${name}, are you for sure for sure you want to take this quiz?  It might be REALLY hard...`);
+    if (confirmation === false) return;
+    const answer1 = prompt('Do I like P5.js?');
+    const answer2 = prompt('Do I like narcs?');
+    const answer3 = prompt('Am I indeed a human?');
+    let count = 0;
+    if (isYes(answer1)) count += 1;
+    if (!isYes(answer2)) count += 1;
+    if (isYes(answer3)) count += 1;
+    total.textContent=`${name}, you got ${count} correct, that's pretty good, I guess...`;
+});
---
+++ test/example.test.js
@@ -0,0 +1,21 @@
+// // IMPORT MODULES under test here:
+// import addThenDouble from '../addThenDouble.js';
+
+// const test = QUnit.test;
+
+// test('time to test a function', function(assert) {
+//     //Arrange
+//     // Set up your parameters and expectations
+//     const shouldBe12 = addThenDouble(2, 4); // 2+4 =6, 6*2 = 12
+
+
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+
+//     //Assert
+//     // Make assertions about what is expected valid result
+//     assert.equal(shouldBe12, 12);
+
+//     const shouldBe40 = addThenDouble(18, 2);
+//     assert.equal(addThenDouble, )
+// });
