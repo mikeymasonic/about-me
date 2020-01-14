c67a300c96b70791d53f4325d50f7909f2e53e2e
Michael Romay <amerror@gmail.com>
2020-01-14 11:58:21 -0800

    testing hidden results

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 import isYes from './is-yes.js';
 
 const quizButton = document.getElementById('quizbutton');
-const total = document.getElementById('total');
+const totalGood = document.getElementById('totalGood');
+const totalBad = document.getElementById('totalBad');
+let count = 0;
+
+
+// const hideResults = () => {
+//     Document.getElementById('results').style.display = 'none';
+// };
+
+// quizButton.addEventListener('click', () => {
+//     hideResults();
+// });
 
 quizButton.addEventListener('click', () => {
     // console.log('button works');
+
+    // if (Document.getElementById('resultsBad').style.display) = 'block') {
+    //     Document.getElementById('resultsBad').style.display = 'none';
+    // };
+
+    // Document.getElementById('results').style.display = 'none';
+    // Document.getElementById('resultsBad').style.display = 'none';
+    // hideResults();
+    if (count >= 1) {
+        count = 0;   
+    }
+    // document.getElementById('results').style.display = 'none';
     const name = prompt('What\'s yer name?');
     const confirmation = confirm(`${name}, are you for sure for sure you want to take this quiz?  It might be REALLY hard...`);
     if (confirmation === false) return;
     const answer1 = prompt('Do I like P5.js?');
     const answer2 = prompt('Do I like narcs?');
     const answer3 = prompt('Am I indeed a human?');
-    let count = 0;
+    // let count = 0;
     if (isYes(answer1)) count += 1;
     if (!isYes(answer2)) count += 1;
     if (isYes(answer3)) count += 1;
-    total.textContent = `${name}, you got ${count} correct, that's pretty good, I guess...`;
+
+    if (count >= 2) {
+        totalGood.textContent = `${name}, you got ${count} correct, that's pretty good, I guess...`;
+        document.getElementById('resultsGood').style.display = 'block';
+        document.getElementById('results').style.display = 'block';
+    } else {
+        totalBad.textContent = `${name}, you got ${count} correct, that was terrible!`;
+        document.getElementById('resultsBad').style.display = 'block';
+        document.getElementById('results').style.display = 'block';
+    }
+    
 });
+
